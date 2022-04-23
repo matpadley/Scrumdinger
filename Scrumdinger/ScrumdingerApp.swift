c12d97d46b8580993fbcf58e13e35df31c659bff
matpadley <mrpadley@icloud.com>
2022-04-23 08:56:24 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mathew Padley on 23/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
