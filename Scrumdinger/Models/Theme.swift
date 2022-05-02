264716a43367d7efcca17f8fd348645c2acc52c5
matpadley <mrpadley@icloud.com>
2022-05-02 15:28:50 +0100

    Card View and Accessibility

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,40 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Mathew Padley on 02/05/2022.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColour: Color {
+        switch self {
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
+                return .black
+            case .indigo, .magenta, .navy, .oxblood, .purple:
+                return .white
+        }
+    }
+    
+    var mainColour: Color{
+        Color(rawValue)
+    }
+}
